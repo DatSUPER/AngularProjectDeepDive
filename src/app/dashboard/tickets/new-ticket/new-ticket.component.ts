7039ec69f1f337310ad3aa83f7262c6148e3f290
DatSUPER <98950270+DatSUPER@users.noreply.github.com>
2024-07-30 18:27:01 +0200

    you can add ticket to tickets

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-new-ticket',
@@ -10,10 +11,29 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
-  onSubmit(titleElement: HTMLInputElement){
-    console.dir(titleElement);
-    const enteredTitle = titleElement.value;
-    console.log(enteredTitle)
+export class NewTicketComponent implements OnInit, AfterViewInit{
+
+@ViewChild("form") form?: ElementRef<HTMLFormElement>;
+// private form = viewChild.required<ElementRef<HTMLFormElement>>("form");
+// @Output() add = new EventEmitter();
+add = output<{title: string; text: string}>();
+
+
+  ngOnInit(){
+      console.log("ONINIT");
+      console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit(){
+      console.log("after view init");
+      console.log(this.form?.nativeElement);
+      
+  }
+
+
+  onSubmit(title: string, ticketText: string){
+
+    this.add.emit({title: title, text: ticketText});
+    this.form?.nativeElement.reset();
   }
 }
